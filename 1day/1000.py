68a2a35a4bb7e6f6b2d54d592dafb17a7950ac71
KimGyeongHyeon <kkl5563@naver.com>
2025-07-06 21:13:35 +0900

    1 day

---
+++ 1day/1000.py
@@ -0,0 +1,10 @@
+A, B = input().split()
+x = int(A)
+y = int(B)
+print(x+y)
+
+# split() : 문자열을 공백이나 다른 문자로 분리해서 리스트를 반환, 공백기준으로 앞 뒤 나눔
+# 2번째 방법으로 map함수가 있음
+# a,b = map(int, input().split())
+# print(a+b)
+# 리스트 하나하나를 함수명에 대입
